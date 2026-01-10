4fdc09cf428966eac1212aa4253334258a391500
nrlgarozzo <164266161+nrlgarozzo@users.noreply.github.com>
2026-01-09 23:27:54 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
+import { Howl } from "https://unpkg.com/howler@2.2.4/dist/howler.module.js";
 
+const sound = new Howl({
+  src: ["https://ko-audio.howtopronounce.com/26ce399e723333542fa6a4c798b8879b.mp3"]
+});
+
+document.getElementById("play").onclick = () => sound.play();
