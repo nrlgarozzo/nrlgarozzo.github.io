41e5ddfe43337d380dcf892613eee78876ac5949
nrlgarozzo <164266161+nrlgarozzo@users.noreply.github.com>
2026-01-10 10:44:54 -0600

    Update main.js

--- main.js
+++ main.js
@@ -14,6 +14,8 @@ Papa.parse("ape.csv", {
 let randomNum = Math.random();
 console.log(randomNum);
 
+console.log(data);
+
 
 const sound = new Howl({
   src: ["https://ko-audio.howtopronounce.com/26ce399e723333542fa6a4c798b8879b.mp3"], 
