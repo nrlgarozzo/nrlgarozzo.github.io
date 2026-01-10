563012c36c42a5e79ada11b47363631cd34a878a
nrlgarozzo <164266161+nrlgarozzo@users.noreply.github.com>
2026-01-10 01:32:42 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
-import { Howl } from "https://unpkg.com/howler@2.2.4/dist/howler.module.js";
+//import { Howl } from "https://unpkg.com/howler@2.2.4/dist/howler.module.js";
 
 //console.log("Button found?", document.querySelector(".button-b"));
 
-const sound = new Howl({
+//const sound = new Howl({
   //src: ["https://ko-audio.howtopronounce.com/26ce399e723333542fa6a4c798b8879b.mp3"], 
-  src: ["https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"],
-  preload: true, 
-  html5: false,
-});
+//  src: ["https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"],
+//  preload: true, 
+//  html5: false,
+//});
 
 //document.getElementById("play").onclick = () => sound.play();
 
-function playsound() {
-  sound.play();
-}
+//function playsound() {
+//  sound.play();
+//}
 
 //document.querySelector(".button-b").addEventListener("click", playsound)
 
-document.addEventListener("DOMContentLoaded", () => {
-  const button = document.querySelector(".button-b");
+//document.addEventListener("DOMContentLoaded", () => {
+//  const button = document.querySelector(".button-b");
   //button.addEventListener("click", playSound);
-  button.addEventListener("click", () => {
+//  button.addEventListener("click", () => {
                 // Redirect to another page
-                window.location.href = "https://www.youtube.com";
-            });
-});
+//                window.location.href = "https://www.youtube.com";
+//            });
+//});
