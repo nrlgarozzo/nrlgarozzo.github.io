30758472fd167b738069d8a0a7833d7a71a9b947
nrlgarozzo <164266161+nrlgarozzo@users.noreply.github.com>
2026-01-09 23:40:49 -0600

    Update main.js

--- main.js
+++ main.js
@@ -4,4 +4,6 @@ const sound = new Howl({
   src: ["https://ko-audio.howtopronounce.com/26ce399e723333542fa6a4c798b8879b.mp3"]
 });
 
-document.getElementById("play").onclick = () => sound.play();
+//document.getElementById("play").onclick = () => sound.play();
+
+document.querySelector(".button-b").addEventListener("click, () => {sound.play()}")
