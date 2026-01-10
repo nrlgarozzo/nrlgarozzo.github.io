55606ed73d4c0f199b6b56ca6a8002a89f20dc7c
nrlgarozzo <164266161+nrlgarozzo@users.noreply.github.com>
2026-01-10 16:51:18 -0600

    Update main.js

--- main.js
+++ main.js
@@ -2,11 +2,9 @@ console.log("main.js loaded");
 
 let csv_data; //loads empty variable
 let csv_meta;
+let csvResolve;
 
-function CallBack() {
-      console.log(csv_data);
-      console.log(csv_data[0]['kword'])
-}
+const csvready = new Promise(resolve => {csvResole = resolve}); //defining a constant with three states: pending, fulfilled, rejected
 
 Papa.parse("ape.csv", {
   download: true,
@@ -15,19 +13,31 @@ Papa.parse("ape.csv", {
     csv_data = results.data;
     csv_meta = results.meta;
 
-    CallBack();  
+    csvResolve(); //this function switches csvready to the  'fulfilled' state and tells the other functions to proceed which are waiting
     
   }
 });
 
-let randomNum = Math.random();
-console.log(randomNum);
+//let randomNum = Math.random();
+//console.log(randomNum);
+
+async function bruh() {
+      await csvready
+      console.log(csv_data);
+      console.log(csv_data[0]['kword'])
+}
+
+bruh()
 
 //console.log(csv_data);
 //console.log(csv_data[0])
 //console.log(csv_data[0]['kword'])
 //console.log(csv_meta)
 
+
+//generate new key
+//repeat sound button
+//give answer button
 
 const sound = new Howl({
   src: ["https://ko-audio.howtopronounce.com/26ce399e723333542fa6a4c798b8879b.mp3"], 
